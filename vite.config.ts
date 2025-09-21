4d767acde9f6bab3b01621a220d898c3aed47c09
TYeshua <thiagoyeshua01@gmail.com>
2025-09-21 01:16:03 -0300

    aaaaaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/LIRGE-CLAUDIO/', // ✅ caminho correto para GitHub Pages
+  base: './', // relativo
   plugins: [react()],
-});
+})
